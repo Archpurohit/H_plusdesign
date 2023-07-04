7236931d745e6f78099e541a6ac92bc6a347c247
Archit Purohit <Architpurohit@hotmail.com>
2023-07-04 15:33:38 -0700

    Links for Confidental properties

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
-import Header from './Header';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'; import Header from './Header';
 import Services from './Services';
 import Home from './Home';
 import Contact from './Contact';
 import Commercial from './Commercial';
-import Residental from './Residental';
-import Footer from './Footer';
-import ResidentalCoquitlam1 from './ResidentalCoquitlam1';
-import VancouverCondo from './VancouverCondo';
-import KitchenReno from './KitchenReno';
-import AlaskaHome from './AlaskaHome';
-import ResidentalCoquitlam2 from './ResidentalCoquitlam2';
-import Renovations from './Renovations';
-
+import Residental from './Residental'; import Footer from './Footer'; import ResidentalCoquitlam1 from './ResidentalCoquitlam1'; import VancouverCondo from './VancouverCondo'; import KitchenReno from './KitchenReno'; import AlaskaHome from './AlaskaHome'; import ResidentalCoquitlam2 from './ResidentalCoquitlam2'; import Renovations from './Renovations'; import SBIBank from './SBIBank'; import ICICIBank from './ICICIBank'; import Veerji from './Veerji'; import Gulley from './Gulley';
+import GoGrill from './GoGrill';
+import MumbaiLocal from './MumbaiLocal';
+import NYGrill from './NYGrill';
+import RebelBoxing from './RebelBoxing';
+import Notary from './Notary';
+import HiFive from './HiFive';
 
 
 function App() {
@@ -25,17 +22,32 @@ function App() {
 
           <div className="main">
             <Routes>
+              {/* home page links */}
+              <Route path="/" element={<Home />} />
+              <Route path="/Services" element={<Services />} />
+              <Route path="/Commercial" element={<Commercial />} />
+              <Route path="/Residental" element={<Residental />} />
               <Route path="/Contact" element={<Contact />} />
+
+              {/* Residentail links */}
               <Route path="/ResidentalCoquitlam1" element={<ResidentalCoquitlam1 />} />
               <Route path="/ResidentalCoquitlam2" element={<ResidentalCoquitlam2 />} />
               <Route path="/VancouverCondo" element={<VancouverCondo />} />
-              <Route path="/" element={<Home />} />
               <Route path="/AlaskaHome" element={<AlaskaHome />} />
               <Route path="/KitchenReno" element={<KitchenReno />} />
               <Route path="/Renovations" element={<Renovations />} />
-              <Route path="/Commercial" element={<Commercial />} />
-              <Route path="/Residental" element={<Residental />} />
-              <Route path="/Services" element={<Services />} />
+
+              {/* Commerical links */}
+              <Route path="/SBIBank" element={<SBIBank />} />
+              <Route path="/ICICIBank" element={<ICICIBank />} />
+              <Route path="/Veerji" element={<Veerji />} />
+              <Route path="/Gulley" element={<Gulley />} />
+              <Route path="/GoGrill" element={<GoGrill />} />
+              <Route path="/MumbaiLocal" element={<MumbaiLocal />} />
+              <Route path="/NYGrill" element={<NYGrill />} />
+              <Route path="/HiFive" element={<HiFive />} />
+              <Route path="/RebelBoxing" element={<RebelBoxing />} />
+              <Route path="/Notary" element={<Notary />} />
             </Routes>
           </div>
 
--- src/Commercial.js
+++ src/Commercial.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Commercial.css'
-
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 export default function Commercial() {
   return (
@@ -10,57 +10,77 @@ export default function Commercial() {
 
       <div className='commercialBox1'>
 
+
+      <Link to="/SBIBank">
       <div className='Projects1'>
         <div className='picture' id='picture1'></div>
         <div className='ProjectName'> <h3>SBI Bank Surrey</h3></div>
-      </div>
+        </div>
+        </Link>
 
+      <Link to="/ICICIBank">
       <div className='Projects1'>
         <div className='picture'id='picture2'></div>
         <div className='ProjectName' > <h3>ICICI Bank Delta</h3></div>
       </div>
+      </Link>
 
+      <Link to="/Veerji">
       <div className='Projects1'>
         <div className='picture'  id='picture3'></div>
         <div className='ProjectName'> <h3>Veerji Fish and Grill</h3></div>
       </div>
+      </Link>
 
+      <Link to="/Gulley">
       <div className='Projects1'>
         <div className='picture' id='picture4'></div>
         <div className='ProjectName' ><h3>Gulley Canteen Coquitlam</h3>
         </div>
       </div>
+      </Link>
 
-
+      <Link to="/GoGril">
       <div className='Projects1'>
         <div className='picture' id='picture5'></div>
         <div className='ProjectName' ><h3>Go Grill Abbotsford </h3></div>
       </div>
+      </Link>
+
+      <Link to="/MumbaiLocal">
       <div className='Projects1'>
         <div className='picture'  id='picture6'></div>
         <div className='ProjectName'> <h3>Mumbai Local</h3></div>
       </div>
+      </Link>
 
+      <Link to="/NYGrill">
       <div className='Projects1'>
         <div className='picture' id='picture7'></div>
         <div className='ProjectName' > <h3>NY Grill Langley</h3></div>
       </div>
+      </Link>
 
+      <Link to="/HiFive">
       <div className='Projects1'>
         <div className='picture' id='picture8'></div>
         <div className='ProjectName' > <h3>Hi Five Burnaby</h3></div>
       </div>
+      </Link>
 
+      <Link to="/RebelBoxing">
       <div className='Projects1'>
         <div className='picture' id='picture9'></div>
         <div className='ProjectName' > <h3>Rebel Boxing Studio Coquitlam</h3></div>
       </div>
+      </Link>
 
-
+      <Link to="/Notary">
       <div className='Projects1'>
         <div className='picture' id='picture10'></div>
         <div className='ProjectName' > <h3>Surrey Notary Office</h3></div>
       </div>
+      </Link>
 
 
       </div>
